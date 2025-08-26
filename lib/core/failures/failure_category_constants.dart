6a2f33c64c20a94b6c774c672a0933e4a187fc68
Anton <anton@antons-webfabrik.eu>
2025-08-26 19:54:34 +0200

    feat(core): add FailureCategoryConstants for categorizing failures

---
+++ lib/core/failures/failure_category_constants.dart
@@ -0,0 +1,7 @@
+class FailureCategoryConstants {
+  static const String general = 'general';
+  static const String authentication = 'authentication';
+  static const String networking = 'networking';
+  static const String storage = 'storage';
+  static const String permission = 'permission';
+}
