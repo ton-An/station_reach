7837a194c85abd1adc40e0c1821cc075eaa13715
Anton <anton@antons-webfabrik.eu>
2025-08-31 16:30:56 +0200

    feat(models): update Station model to conform to otp api

--- lib/models/station.dart
+++ lib/models/station.dart
@@ -4,19 +4,39 @@ class Station {
     required this.name,
     required this.latitude,
     required this.longitude,
+    required this.childrenIds,
   });
 
   final String id;
   final String name;
   final double latitude;
   final double longitude;
+  final List<String> childrenIds;
 
-  factory Station.fromJson(Map<String, dynamic> json) {
+  factory Station.fromJson(Map json) {
     return Station(
-      id: json['id'],
-      name: json['name'],
-      latitude: json['location']['latitude'],
-      longitude: json['location']['longitude'],
+      id: json['primary']?['id'] ?? json['id'] ?? json['gtfsId'],
+      name: json['description'] ?? json['name'] ?? json['primary']?['name'],
+      latitude:
+          json['lat'] ?? json['lat'] ?? json['primary']?['coordinate']['lat'],
+      longitude:
+          json['lon'] ?? json['lon'] ?? json['primary']?['coordinate']['lon'],
+      childrenIds:
+          json['secondaries']
+              ?.map((secondary) => secondary['id'])
+              .toList()
+              .cast<String>() ??
+          [],
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'latitude': latitude,
+      'longitude': longitude,
+      'childrenIds': childrenIds,
+    };
   }
 }
