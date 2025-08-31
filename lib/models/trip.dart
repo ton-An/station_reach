bc602750ecdb4411a7a7bbd6b02534395261ba3c
Anton <anton@antons-webfabrik.eu>
2025-08-31 16:30:36 +0200

    refactor(core): update Trip model to conform to otp api

--- lib/models/trip.dart
+++ lib/models/trip.dart
@@ -1,25 +1,51 @@
-import 'package:station_reach/models/station.dart';
+import 'package:station_reach/models/reachable_station.dart';
 
 class Trip {
   Trip({required this.id, required this.name, required this.stops});
 
   final String id;
   final String name;
-  final List<Station> stops;
+  final List<ReachableStation> stops;
 
-  factory Trip.fromJson(Map<String, dynamic> json) {
+  factory Trip.fromJson(Map json) {
+    final Map<String, dynamic> trip = json['stoptimes'][0];
+
+    final Duration departureTime = Duration(
+      seconds: trip['scheduledDeparture'],
+    );
+
+    final List<Map> stops = trip['trip']['stoptimes'];
+
+    final List<ReachableStation> computedStops = <ReachableStation>[];
+
+    for (final Map stop in stops) {
+      final stopArrivalTime = Duration(seconds: stop['scheduledArrival']);
+
+      if (stopArrivalTime < departureTime) {
+        continue;
+      }
+
+      final String stopId = stop['stop']['gtfsId'];
+      final String stopName = stop['stop']['name'];
+      final double stopLatitude = stop['stop']['lat'];
+      final double stopLongitude = stop['stop']['lon'];
+
+      computedStops.add(
+        ReachableStation(
+          id: stopId,
+          name: stopName,
+          latitude: stopLatitude,
+          longitude: stopLongitude,
+          childrenIds: [],
+          duration: stopArrivalTime - departureTime,
+        ),
+      );
+    }
+
     return Trip(
-      id: json['tripId'],
-      name: json['line']['name'],
-      stops: (json['nextStopovers'] as List)
-          .where(
-            (stop) =>
-                stop['stop']['id'] != null &&
-                stop['stop']['name'] != null &&
-                stop['stop']['location'] != null,
-          )
-          .map((stop) => Station.fromJson(stop['stop']))
-          .toList(),
+      id: json['pattern']['id'],
+      name: json['pattern']['route']['shortName'],
+      stops: computedStops,
     );
   }
 }
