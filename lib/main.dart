52007b9e9590b12a11638395b8fe32ccb7decb55
Anton <anton@antons-webfabrik.eu>
2025-08-31 16:28:05 +0200

    feat(core): add timeline gradient colors

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,15 @@ class _MainAppState extends State<MainApp> {
           primaryTranslucent: const Color.fromARGB(60, 83, 196, 108),
           accent: Color.fromARGB(255, 7, 114, 255),
           translucentBackground: Color(0xFFFFFFFF).withValues(alpha: .95),
+          timelineGradient: [
+            Color.fromARGB(255, 0, 150, 136),
+            Color.fromARGB(255, 76, 175, 80),
+            Color.fromARGB(255, 255, 245, 59),
+            Color.fromARGB(255, 255, 229, 59),
+            Color.fromARGB(255, 255, 152, 0),
+            Color.fromARGB(255, 244, 67, 54),
+            Color.fromARGB(255, 156, 39, 176),
+          ],
         ),
         text: WebfabrikTextThemeData(
           largeTitle: TextStyle(
