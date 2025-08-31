499f75aa7ab6f702b37c497c84fa83645bf14323
Anton <anton@antons-webfabrik.eu>
2025-08-31 16:29:44 +0200

    feat(map): add _Legends widget for displaying map legends

---
+++ lib/pages/map_page/_legends.dart
@@ -0,0 +1,25 @@
+part of 'map_page.dart';
+
+class _Legends extends StatelessWidget {
+  const _Legends();
+
+  @override
+  Widget build(BuildContext context) {
+    final WebfabrikThemeData theme = WebfabrikTheme.of(context);
+
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Padding(
+        padding: EdgeInsets.only(
+          bottom: theme.spacing.medium,
+          left: theme.spacing.medium,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [_ScaleBar(), XSmallGap(), _TimeGradientLegend()],
+        ),
+      ),
+    );
+  }
+}
