6ca408f126d6cba4e1615535469878b9dd66e6ce
Anton <anton@antons-webfabrik.eu>
2025-09-03 14:01:03 +0200

    feat(map): make AttributionLegend expandable

--- lib/pages/map_page/_attribution_legend.dart
+++ lib/pages/map_page/_attribution_legend.dart
@@ -1,59 +1,116 @@
 part of 'map_page.dart';
 
-class _AttributionLegend extends StatelessWidget {
+class _AttributionLegend extends StatefulWidget {
   const _AttributionLegend();
+
+  @override
+  State<_AttributionLegend> createState() => _AttributionLegendState();
+}
+
+class _AttributionLegendState extends State<_AttributionLegend> {
+  bool _expanded = false;
 
   @override
   Widget build(BuildContext context) {
     final WebfabrikThemeData theme = WebfabrikTheme.of(context);
 
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(theme.radii.small),
-      child: BackdropFilter(
-        filter: theme.misc.blurFilter,
-        child: Container(
-          padding: EdgeInsets.all(theme.spacing.xxSmall),
-          color: theme.colors.translucentBackground,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _AttributionLink(
-                text: AppLocalizations.of(context)!.openStreetMapAttribution,
-                url: 'https://www.openstreetmap.org/copyright',
+    return Padding(
+      padding: EdgeInsets.only(left: theme.spacing.xxSmall),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(theme.radii.small),
+        child: BackdropFilter(
+          filter: theme.misc.blurFilter,
+          child: Container(
+            color: theme.colors.translucentBackground,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              reverse: true,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  if (_expanded)
+                    Padding(
+                      padding: EdgeInsets.only(left: theme.spacing.xxSmall),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          _AttributionLink(
+                            text: AppLocalizations.of(
+                              context,
+                            )!.openStreetMapAttribution,
+                            url: 'https://www.openstreetmap.org/copyright',
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: theme.spacing.xSmall,
+                            ),
+                            child: const Dot(),
+                          ),
+                          _AttributionLink(
+                            text: AppLocalizations.of(
+                              context,
+                            )!.mapTilerAttribution,
+                            url: 'https://www.maptiler.com/',
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: theme.spacing.xSmall,
+                            ),
+                            child: const Dot(),
+                          ),
+                          _AttributionLink(
+                            text: AppLocalizations.of(
+                              context,
+                            )!.mapLibreAttribution,
+                            url: 'https://maplibre.org/',
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: theme.spacing.xSmall,
+                            ),
+                            child: const Dot(),
+                          ),
+                          _AttributionLink(
+                            text: AppLocalizations.of(
+                              context,
+                            )!.dataSourcesAttribution,
+                            url: 'https://transitous.org/sources/',
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: theme.spacing.xSmall,
+                            ),
+                            child: const Dot(),
+                          ),
+                          _AttributionLink(
+                            text: AppLocalizations.of(
+                              context,
+                            )!.transitousAttribution,
+                            url: 'https://transitous.org/',
+                          ),
+                        ],
+                      ),
+                    ),
+
+                  GestureDetector(
+                    behavior: HitTestBehavior.translucent,
+                    onTap: () {
+                      setState(() {
+                        _expanded = !_expanded;
+                      });
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.all(theme.spacing.xxSmall),
+                      child: Icon(
+                        _expanded ? Icons.close_rounded : Icons.info_rounded,
+                        color: theme.colors.hint,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: theme.spacing.xSmall),
-                child: const Dot(),
-              ),
-              _AttributionLink(
-                text: AppLocalizations.of(context)!.mapTilerAttribution,
-                url: 'https://www.maptiler.com/',
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: theme.spacing.xSmall),
-                child: const Dot(),
-              ),
-              _AttributionLink(
-                text: AppLocalizations.of(context)!.mapLibreAttribution,
-                url: 'https://maplibre.org/',
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: theme.spacing.xSmall),
-                child: const Dot(),
-              ),
-              _AttributionLink(
-                text: AppLocalizations.of(context)!.dataSourcesAttribution,
-                url: 'https://transitous.org/sources/',
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: theme.spacing.xSmall),
-                child: const Dot(),
-              ),
-              _AttributionLink(
-                text: AppLocalizations.of(context)!.transitousAttribution,
-                url: 'https://transitous.org/',
-              ),
-            ],
+            ),
           ),
         ),
       ),
