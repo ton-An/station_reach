e2fae49ca7e593684f5eed4e3fe3b4d282cda180
Anton <anton@antons-webfabrik.eu>
2025-09-07 12:21:38 +0200

    refactor(map): impl searchStations in data source

--- lib/features/map/data/datasources/map_remote_data_source.dart
+++ lib/features/map/data/datasources/map_remote_data_source.dart
@@ -1,5 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:station_reach/core/constants.dart';
 import 'package:station_reach/features/map/domain/models/station.dart';
 
 abstract class MapRemoteDataSource {
   Future<List<Station>> searchStations({required String query});
+}
+
+class MapRemoteDataSourceImpl extends MapRemoteDataSource {
+  MapRemoteDataSourceImpl({required this.dio});
+
+  final Dio dio;
+
+  @override
+  Future<List<Station>> searchStations({required String query}) async {
+    final Uri url = Uri.parse(
+      '${Constants.otpUrl}geocode/stopClusters?query=$query',
+    );
+
+    final Response response = await dio.getUri(url);
+
+    final List<Station> stations = response.data.map((location) {
+      return Station.fromJson(location);
+    }).toList();
+
+    return stations;
+  }
 }
