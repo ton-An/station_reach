cb2940766a276ac5da76d5c7d07456f234bd518f
Anton <anton@antons-webfabrik.eu>
2025-10-21 20:36:24 +0200

    chore: re-add pointer interceptor package

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import geolocator_apple
-import maplibre
-import package_info_plus
 import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  MaplibrePlugin.register(with: registry.registrar(forPlugin: "MaplibrePlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
