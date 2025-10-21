bd7c37e35c6482bf2e465d5790e92704869942b6
Anton <anton@antons-webfabrik.eu>
2025-10-21 17:53:31 +0200

    feat: implement transitous transit modes

--- lib/features/map/domain/enums/transit_mode.dart
+++ lib/features/map/domain/enums/transit_mode.dart
@@ -1,52 +1,96 @@
+/*
+  To-Do:
+    - [ ] Remove transit modes that don't apply to the app
+*/
+
 enum TransitMode {
-  airplane,
-  bus,
-  cableCar,
-  carpool,
-  coach,
-  ferry,
-  funicular,
-  gondola,
-  monorail,
-  rail,
-  subway,
-  taxi,
+  walk,
+  bike,
+  rental,
+  car,
+  carParking,
+  carDropoff,
+  odm,
+  flex,
+  transit,
   tram,
-  trolleybus,
-  unknown;
+  subway,
+  ferry,
+  airplane,
+  suburban,
+  bus,
+  coach,
+  rail,
+  highspeedRail,
+  longDistance,
+  nightRail,
+  regionalFastRail,
+  regionalRail,
+  cableCar,
+  funicular,
+  aerialLift,
+  arealLift,
+  metro,
+  other;
 
   static TransitMode fromString(String value) {
     switch (value) {
-      case 'AIRPLANE':
-        return TransitMode.airplane;
-      case 'BUS':
-        return TransitMode.bus;
-      case 'CABLE_CAR':
-        return TransitMode.cableCar;
-      case 'CARPOOL':
-        return TransitMode.carpool;
-      case 'COACH':
-        return TransitMode.coach;
-      case 'FERRY':
-        return TransitMode.ferry;
-      case 'FUNICULAR':
-        return TransitMode.funicular;
-      case 'GONDOLA':
-        return TransitMode.gondola;
-      case 'MONORAIL':
-        return TransitMode.monorail;
-      case 'RAIL':
-        return TransitMode.rail;
-      case 'SUBWAY':
-        return TransitMode.subway;
-      case 'TAXI':
-        return TransitMode.taxi;
+      case 'WALK':
+        return TransitMode.walk;
+      case 'BIKE':
+        return TransitMode.bike;
+      case 'RENTAL':
+        return TransitMode.rental;
+      case 'CAR':
+        return TransitMode.car;
+      case 'CAR_PARKING':
+        return TransitMode.carParking;
+      case 'CAR_DROPOFF':
+        return TransitMode.carDropoff;
+      case 'ODM':
+        return TransitMode.odm;
+      case 'FLEX':
+        return TransitMode.flex;
+      case 'TRANSIT':
+        return TransitMode.transit;
       case 'TRAM':
         return TransitMode.tram;
-      case 'TROLLEYBUS':
-        return TransitMode.trolleybus;
+      case 'SUBWAY':
+        return TransitMode.subway;
+      case 'FERRY':
+        return TransitMode.ferry;
+      case 'AIRPLANE':
+        return TransitMode.airplane;
+      case 'SUBURBAN':
+        return TransitMode.suburban;
+      case 'BUS':
+        return TransitMode.bus;
+      case 'COACH':
+        return TransitMode.coach;
+      case 'RAIL':
+        return TransitMode.rail;
+      case 'HIGHSPEED_RAIL':
+        return TransitMode.highspeedRail;
+      case 'LONG_DISTANCE':
+        return TransitMode.longDistance;
+      case 'NIGHT_RAIL':
+        return TransitMode.nightRail;
+      case 'REGIONAL_FAST_RAIL':
+        return TransitMode.regionalFastRail;
+      case 'REGIONAL_RAIL':
+        return TransitMode.regionalRail;
+      case 'CABLE_CAR':
+        return TransitMode.cableCar;
+      case 'FUNICULAR':
+        return TransitMode.funicular;
+      case 'AERIAL_LIFT':
+        return TransitMode.aerialLift;
+      case 'AREAL_LIFT':
+        return TransitMode.arealLift;
+      case 'METRO':
+        return TransitMode.metro;
       default:
-        return TransitMode.unknown;
+        return TransitMode.other;
     }
   }
 }
