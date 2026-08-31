28b9ed93e8b28546da0fd83ad787f3bb92606e84
Anton <anton@antons-webfabrik.eu>
2026-08-31 16:37:08 +0200

    chore: add import sort + unused remover

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 const prettierConfig = require('eslint-config-prettier');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
+const unusedImports = require('eslint-plugin-unused-imports');
 const catchMustRethrow = require('./eslint-rules/catch-must-rethrow');
 
 module.exports = defineConfig([
@@ -15,9 +17,14 @@ module.exports = defineConfig([
           'catch-must-rethrow': catchMustRethrow,
         },
       },
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
     },
     rules: {
       'local/catch-must-rethrow': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'unused-imports/no-unused-imports': 'error',
     },
   },
   {
